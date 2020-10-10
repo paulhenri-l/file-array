7e87ea1f66e68b437da280aff3245b3837e686ea
paulhenri-l <25308170+paulhenri-l@users.noreply.github.com>
2020-10-10 23:44:38 +0200

    Drop BucketManager

--- src/FileArray.php
+++ src/FileArray.php
@@ -2,35 +2,106 @@
 
 namespace PaulhenriL\FileArray;
 
-use PaulhenriL\FileArray\Interfaces\BucketManagerInterface;
+use PaulhenriL\FileArray\InMemory\InMemoryBucketFactory;
+use PaulhenriL\FileArray\Interfaces\BucketFactoryInterface;
+use PaulhenriL\FileArray\Interfaces\BucketInterface;
 
 class FileArray implements \ArrayAccess
 {
-    /** @var BucketManagerInterface */
-    protected $bucketManager;
+    protected $buckets = [];
 
-    public function __construct(BucketManagerInterface $bucketManager = null)
-    {
-        $this->bucketManager = $bucketManager ?? new BucketManager();
+    /** @var int */
+    protected $numberOfBuckets;
+
+    /** @var BucketFactoryInterface */
+    protected $bucketFactory;
+
+    public function __construct(
+        int $numberOfBuckets = 100,
+        ?BucketFactoryInterface $bucketFactory = null
+    ) {
+        $this->numberOfBuckets = $numberOfBuckets;
+        $this->bucketFactory = $bucketFactory ?? new InMemoryBucketFactory();
     }
 
-    public function offsetExists($key)
+    public function offsetExists($offset)
     {
-        return $this->bucketManager->hasKey($key);
+        return $this->hasKey($offset);
     }
 
-    public function offsetGet($key)
+    public function offsetGet($offset)
     {
-        return $this->bucketManager->get($key);
+        return $this->get($offset);
     }
 
-    public function offsetSet($key, $value)
+    public function offsetSet($offset, $value)
     {
-        $this->bucketManager->set($key, $value);
+        $this->set($offset, $value);
     }
 
-    public function offsetUnset($key)
+    public function offsetUnset($offset)
     {
-        $this->bucketManager->unset($key);
+        $this->unset($offset);
+    }
+
+    public function get(string $key)
+    {
+        return $this->getBucketFor($key)->get($key);
+    }
+
+    public function set(string $key, $value): void
+    {
+        $bucket = $this->getBucketFor($key);
+
+        $bucket->set($key, $value);
+
+        $this->setBucketFor($key, $bucket);
+    }
+
+    public function hasKey(string $key): bool
+    {
+        return $this->getBucketFor($key)->hasKey($key);
+    }
+
+    public function unset(string $key): void
+    {
+        $bucket = $this->getBucketFor($key);
+
+        $bucket->unset($key);
+
+        if ($bucket->length() === 0) {
+            $this->removeBucketFor($key);
+        }
+    }
+
+    public function getBuckets(): array
+    {
+        return $this->buckets;
+    }
+
+    protected function getBucketFor(string $key): BucketInterface
+    {
+        $hash = $this->getBucketHashForKey($key);
+
+        return $this->buckets[$hash] ?? $this->bucketFactory->newBucket($hash);
+    }
+
+    protected function setBucketFor(string $key, BucketInterface $bucket): void
+    {
+        $hash = $this->getBucketHashForKey($key);
+
+        $this->buckets[$hash] = $bucket;
+    }
+
+    protected function removeBucketFor(string $key)
+    {
+        $hash = $this->getBucketHashForKey($key);
+
+        unset($this->buckets[$hash]);
+    }
+
+    protected function getBucketHashForKey(string $key): string
+    {
+        return crc32($key) % $this->numberOfBuckets;
     }
 }
--- tests/Unit/FileArrayTest.php
+++ tests/Unit/FileArrayTest.php
@@ -2,30 +2,14 @@
 
 namespace PaulhenriL\FileArray\Tests\Unit;
 
-use PaulhenriL\FileArray\BucketManager;
-use PaulhenriL\FileArray\Tests\TestCase;
 use PaulhenriL\FileArray\FileArray;
+use PaulhenriL\FileArray\Tests\Fakes\FakeBucket;
+use PaulhenriL\FileArray\Tests\Fakes\FakeBucketFactory;
+use PaulhenriL\FileArray\Tests\TestCase;
 
 class FileArrayTest extends TestCase
 {
-    public function test_construction()
-    {
-        $this->assertInstanceOf(
-            FileArray::class, new FileArray()
-        );
-    }
-
-    public function test_with_custom_bucket_manager()
-    {
-        $bm = new BucketManager();
-
-        $fileArray = new FileArray($bm);
-        $fileArray['hello'] = 'world';
-
-        $this->assertEquals('world', $bm->get('hello'));
-    }
-
-    public function test_set_get()
+    public function test_set_get_array_access()
     {
         $array = new FileArray();
 
@@ -34,7 +18,7 @@ class FileArrayTest extends TestCase
         $this->assertEquals('world', $array['hello']);
     }
 
-    public function test_unset()
+    public function test_unset_array_access()
     {
         $array = new FileArray();
 
@@ -44,7 +28,7 @@ class FileArrayTest extends TestCase
         $this->assertNull($array['hello']);
     }
 
-    public function test_has_key()
+    public function test_has_key_array_access()
     {
         $array1 = new FileArray();
         $array2 = new FileArray();
@@ -55,7 +39,7 @@ class FileArrayTest extends TestCase
         $this->assertFalse(isset($array2['hello']));
     }
 
-    public function test_empty()
+    public function test_empty_array_access()
     {
         $array1 = new FileArray();
         $array2 = new FileArray();
@@ -65,4 +49,89 @@ class FileArrayTest extends TestCase
         $this->assertTrue(empty($array1['hello']));
         $this->assertTrue(empty($array2['hello']));
     }
+
+    public function test_get_defaults_to_false()
+    {
+        $bm = new FileArray();
+
+        $this->assertNull($bm->get('some_key'));
+    }
+
+    public function test_a_key_can_be_set()
+    {
+        $bm = new FileArray();
+
+        $bm->set('some_key', 'some_value');
+
+        $this->assertEquals('some_value', $bm->get('some_key'));
+    }
+
+    public function test_has_key_when_no_key()
+    {
+        $bm = new FileArray();
+
+        $this->assertFalse($bm->hasKey('some_key'));
+    }
+
+    public function test_has_key_when_has_key()
+    {
+        $bm = new FileArray();
+
+        $bm->set('some_key', []);
+
+        $this->assertTrue($bm->hasKey('some_key'));
+    }
+
+    public function test_unset()
+    {
+        $bm = new FileArray();
+        $bm->set('some_key', ['some' => 'value']);
+
+        $bm->unset('some_key');
+
+        $this->assertNull($bm->get('some_key'));
+    }
+
+    public function test_bucket_is_removed_on_unset_if_it_is_empty()
+    {
+        $bm = new FileArray();
+        $bm->set('some_key', 'some_value');
+        $this->assertCount(1, $bm->getBuckets());
+
+        $bm->unset('some_key');
+
+        $this->assertCount(0, $bm->getBuckets());
+    }
+
+    public function test_bucket_is_not_removed_on_unset_if_it_is_not_empty()
+    {
+        $bm = new FileArray(1);
+        $bm->set('some_key1', 'some_value1');
+        $bm->set('some_key2', 'some_value2');
+        $this->assertCount(1, $bm->getBuckets());
+
+        $bm->unset('some_key1');
+
+        $this->assertCount(1, $bm->getBuckets());
+    }
+
+    public function test_get_underlying_buckets()
+    {
+        $bm = new FileArray();
+        $bm->set('hello', 'world');
+
+        $this->assertCount(1, $bm->getBuckets());
+    }
+
+    public function test_bucket_factory_can_be_passed()
+    {
+        $bm = new FileArray(100, new FakeBucketFactory());
+
+        $bm->set('hello', 'world');
+
+        foreach ($bm->getBuckets() as $bucket) {
+            $this->assertInstanceOf(FakeBucket::class, $bucket);
+        }
+    }
 }
+
